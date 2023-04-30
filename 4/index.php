d31719f3eb58b005ad97ddbe023f6cee211c5ca5
Dir_Almery <92209281+DirAlmeri@users.noreply.github.com>
2023-04-30 18:48:19 +0300

    Update index.php

--- 4/index.php
+++ 4/index.php
@@ -209,9 +209,9 @@ else {
   $bio= $_POST['bio'];
 
   // Сохранение в БД.
-$user = 'u52927';
-$pass = '5758562';
-  $db = new PDO('mysql:host=localhost;dbname=u52927', $user, $pass, array(PDO::ATTR_PERSISTENT => true));
+$user = 'u52815';
+$pass = '9601081';
+  $db = new PDO('mysql:host=localhost;dbname=u52815', $user, $pass, array(PDO::ATTR_PERSISTENT => true));
    try {
     $stmt = $db->prepare("INSERT INTO form SET name=:name, email=:email, year=:byear, pol=:pol, limbs=:limbs, bio=:bio");
     $stmt->bindParam(':name', $name);
